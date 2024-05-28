7351dd3e68d5891b4ae4c76d87a648b87cf736c5
Tatiana Mathia <mathiatatiana@gmail.com>
2024-05-28 14:52:10 +0300

    inheritance pt1

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = [] 
     
-    def learn(self):
-        pass
+    def learn(self, knowledge_item):
+        self.knowledge.append(knowledge_item)
+    
+    def show_knowledge(self):
+        return self.knowledge  # Return the knowledge list    
+        
